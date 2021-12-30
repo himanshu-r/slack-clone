74f90e75fe5fe57c16eba02a0fbf304dbc1f3bcc
himanshu-r <himanshureddy3@gmail.com>
2021-12-30 09:07:43 +0530

    Creating SLack APp

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD2GbekeUvDoUALD1zLiVWqqspDjZkA7QA",
+    authDomain: "slack-clone-7465b.firebaseapp.com",
+    projectId: "slack-clone-7465b",
+    storageBucket: "slack-clone-7465b.appspot.com",
+    messagingSenderId: "999038974384",
+    appId: "1:999038974384:web:04521fc505dfc74a937e75",
+    measurementId: "G-M9QDXR20LH"
+  };
