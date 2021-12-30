3156e746c2a3a10ce68a371721239fb938eb8fd6
himanshu-r <himanshureddy3@gmail.com>
2021-12-30 11:52:54 +0530

    Creating SIdebar

--- slack-app-clone/src/App.js
+++ slack-app-clone/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -12,22 +13,34 @@ import {
   Routes
 } from "react-router-dom";
 import Header from './components/Header';
+import styled from 'styled-components';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-     
+
+      <Header />
+      <AppBody>
+        <Sidebar />
 
         <Routes>
 
-          <Route path="/" exact element={<Header/>}/>
+          <Route path="/" exact />
+          {/* Chat Component */}
           
          
         </Routes>
         
+      </AppBody>
      
     </div>
   );
 }
 
 export default App;
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+
+`;
