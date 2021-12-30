46a0842d950dbf1e31d785cc19d128981f60ef10
himanshu-r <himanshureddy3@gmail.com>
2021-12-30 10:23:32 +0530

    Creating Header

--- slack-app-clone/src/App.js
+++ slack-app-clone/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+  Link,
+  useRouteMatch,
+  useParams,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     
-      <h1>Hello</h1>
+     <Router>
+      <>
+        
+
+        <Routes>
+
+          <Route path="/" exact>
+            <h1>This is the Home Page </h1>
+          </Route>
+         
+        </Routes>
+        
+      </>
+    </Router>
     </div>
   );
 }
