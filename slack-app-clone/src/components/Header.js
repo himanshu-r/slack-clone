53b112c218022dd4ac8f8bd39c05381d3dc4f5a3
himanshu-r <himanshureddy3@gmail.com>
2021-12-30 11:33:20 +0530

    Completed Header COmponent

--- slack-app-clone/src/components/Header.js
+++ slack-app-clone/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components';
 import {Avatar} from '@material-ui/core';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Header() {
     return (
@@ -17,8 +19,21 @@ function Header() {
 
 
             </HeaderLeft>
-            {/* Header Searcj */}
+
+
+            {/* Header Search */}
+            <HeaderSearch>
+                <SearchIcon />
+                <input placeholder='Search In Slack' />
+            </HeaderSearch>
+
+
             {/* Header Right */}
+            <HeaderRight>
+                <HelpOutlineIcon />
+
+            </HeaderRight>
+
             
         </HeaderContainer>
     )
@@ -29,6 +44,13 @@ export default Header;
 
 const HeaderContainer = styled.div`
     display: flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-around;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+    color: white;
     
 
     
@@ -39,8 +61,50 @@ const HeaderLeft = styled.div`
     align-items: center;
     margin-left: 20px;
 
+    /* Adding Design to Material Ui Root Icon */
+
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 30px;
+    }
+
 `;
 
 const HeaderAvatar = styled(Avatar)`
 
+    cursor: pointer;
+    :hover {
+        opacity: 0.8;
+    }
+
+`;
+
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    opacity: 1;
+    border-radius: 6px;
+    text-align: center;
+    background-color: #421f44;
+    display: flex;
+    padding: 0 50px;
+    border: 1px solid gray;
+    >input{
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30vw;
+        outline: 0;
+        color: white;
+    }
+`;
+
+const HeaderRight = styled.div `
+    flex: 0.3;
+    display: flex;
+    align-items: flex-end;
+
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 20px;
+    }
 `;
