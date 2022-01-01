e89a1f361d2e8e4787463ab0229c67db379c20de
himanshu-r <himanshureddy3@gmail.com>
2022-01-01 21:23:08 +0530

    Changes to App

--- slack-app-clone/src/firebase.js
+++ slack-app-clone/src/firebase.js
@@ -1,10 +1,17 @@
+import firebase from "firebase"
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyD2GbekeUvDoUALD1zLiVWqqspDjZkA7QA",
     authDomain: "slack-clone-7465b.firebaseapp.com",
     projectId: "slack-clone-7465b",
     storageBucket: "slack-clone-7465b.appspot.com",
-    messagingSenderId: "999038974384",
-    appId: "1:999038974384:web:04521fc505dfc74a937e75",
-    measurementId: "G-M9QDXR20LH"
-  };
+};
+
+const firebaseApp  = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebaseApp.auth.GoogleAuthProvider();
+
+export {db, auth, provider}
