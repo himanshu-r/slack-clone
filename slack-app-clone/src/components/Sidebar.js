0be095755190ff21914776fb316cc031f0524e4d
himanshu-r <himanshureddy3@gmail.com>
2022-01-01 20:55:52 +0530

    Changes to sidebar

--- slack-app-clone/src/components/Sidebar.js
+++ slack-app-clone/src/components/Sidebar.js
@@ -4,6 +4,15 @@ import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import CreateIcon from '@mui/icons-material/Create';
 import SidebarOption from './SidebarOption';
 import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
 
 
 
@@ -24,13 +33,17 @@ function Sidebar() {
             </SidebarHeader>
 
             <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption title="Channel" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & User Groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
+            <hr/>
+            <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
             
         </SidebarContainer>
     )
